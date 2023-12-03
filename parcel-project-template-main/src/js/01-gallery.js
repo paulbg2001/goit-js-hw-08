4cf7a62b554eeaa1bdb943f26dad09bf7c4ecf83
paulbg2001 <paul.baragraeff@ulbsibiu.ro>
2023-12-03 18:45:34 +0200

    sa

--- parcel-project-template-main/src/js/01-gallery.js
+++ parcel-project-template-main/src/js/01-gallery.js
@@ -1,5 +1,38 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  const listItemEl = document.createElement('li');
+  listItemEl.classList.add('gallery__item');
+  listItemEl.innerHTML = `<a class='gallery__link' href='${item.original}'>
+        <img class='gallery__image' 
+        src='${item.preview}' 
+        data-source='${item.original}' 
+        alt='${item.description}'/>
+    </a>`;
+  listEl.append(listItemEl);
+});
+
+listEl.addEventListener('click', openImageInLightbox);
+
+function openImageInLightbox(event) {
+  const clickedOn = event.target;
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  event.preventDefault();
+  basicLightbox
+    .create(
+      `<img width= '1400' heght='900' src='${clickedOn.dataset.source}'/>`
+    )
+    .show();
+}
