1985fc11b21f74d8c4bc6bfc82449bb24f454d22
paulbg2001 <paul.baragraeff@ulbsibiu.ro>
2023-12-03 20:14:41 +0200

    das

--- parcel-project-template-main/src/js/01-gallery.js
+++ parcel-project-template-main/src/js/01-gallery.js
@@ -1,5 +1,3 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
